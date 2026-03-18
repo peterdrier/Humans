75ad224b52e017b36d18c97dc92eb08832ba4609
Peter Drier <peter.drier@gmail.com>
2026-03-18 17:17:30 +0100

    refactor: standardize more shared date display formatting

--- src/Humans.Web/Extensions/DateTimeDisplayExtensions.cs
+++ src/Humans.Web/Extensions/DateTimeDisplayExtensions.cs
@@ -11,11 +11,35 @@ public static class DateTimeDisplayExtensions
     public static string? ToDisplayDate(this DateTime? value) =>
         value?.ToDisplayDate();
 
+    public static string ToDisplayDate(this Instant value) =>
+        value.ToDateTimeUtc().ToDisplayDate();
+
+    public static string? ToDisplayDate(this Instant? value) =>
+        value?.ToDisplayDate();
+
     public static string ToDisplayLongDate(this DateTime value) =>
         value.ToString("d MMMM yyyy", CultureInfo.CurrentCulture);
 
     public static string? ToDisplayLongDate(this DateTime? value) =>
         value?.ToDisplayLongDate();
+
+    public static string ToDisplayLongDate(this Instant value) =>
+        value.ToDateTimeUtc().ToDisplayLongDate();
+
+    public static string? ToDisplayLongDate(this Instant? value) =>
+        value?.ToDisplayLongDate();
+
+    public static string ToDisplayLongDateTime(this DateTime value) =>
+        value.ToString("d MMMM yyyy HH:mm", CultureInfo.CurrentCulture);
+
+    public static string? ToDisplayLongDateTime(this DateTime? value) =>
+        value?.ToDisplayLongDateTime();
+
+    public static string ToDisplayLongDateTime(this Instant value) =>
+        value.ToDateTimeUtc().ToDisplayLongDateTime();
+
+    public static string? ToDisplayLongDateTime(this Instant? value) =>
+        value?.ToDisplayLongDateTime();
 
     public static string ToDisplayDateTime(this DateTime value) =>
         value.ToString("d MMM yyyy HH:mm", CultureInfo.CurrentCulture);
@@ -23,11 +47,35 @@ public static class DateTimeDisplayExtensions
     public static string? ToDisplayDateTime(this DateTime? value) =>
         value?.ToDisplayDateTime();
 
+    public static string ToDisplayDateTime(this Instant value) =>
+        value.ToDateTimeUtc().ToDisplayDateTime();
+
+    public static string? ToDisplayDateTime(this Instant? value) =>
+        value?.ToDisplayDateTime();
+
     public static string ToDisplayCompactDate(this DateTime value) =>
         value.ToString("MMM d, yyyy", CultureInfo.CurrentCulture);
 
+    public static string? ToDisplayCompactDate(this DateTime? value) =>
+        value?.ToDisplayCompactDate();
+
+    public static string ToDisplayCompactDate(this Instant value) =>
+        value.ToDateTimeUtc().ToDisplayCompactDate();
+
+    public static string? ToDisplayCompactDate(this Instant? value) =>
+        value?.ToDisplayCompactDate();
+
     public static string ToDisplayCompactDateTime(this DateTime value) =>
         value.ToString("MMM d, yyyy HH:mm", CultureInfo.CurrentCulture);
+
+    public static string? ToDisplayCompactDateTime(this DateTime? value) =>
+        value?.ToDisplayCompactDateTime();
+
+    public static string ToDisplayCompactDateTime(this Instant value) =>
+        value.ToDateTimeUtc().ToDisplayCompactDateTime();
+
+    public static string? ToDisplayCompactDateTime(this Instant? value) =>
+        value?.ToDisplayCompactDateTime();
 
     public static string ToDisplayTime(this DateTime value) =>
         value.ToString("HH:mm", CultureInfo.InvariantCulture);
@@ -43,9 +91,6 @@ public static class DateTimeDisplayExtensions
 
     public static string ToDisplayGeneralDateTime(this DateTime value) =>
         value.ToString("g", CultureInfo.CurrentCulture);
-
-    public static string ToDisplayCompactDate(this Instant value) =>
-        value.ToDateTimeUtc().ToDisplayCompactDate();
 
     public static string ToAuditTimestamp(this Instant value) =>
         value.ToDateTimeUtc().ToAuditTimestamp();
