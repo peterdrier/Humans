0bf5a65cd5026a1ddfd25d747840e28011934a79
Peter Drier <peter.drier@gmail.com>
2026-04-26 02:39:23 +0200

    feat: add per-camp roles panel view-models

---
+++ src/Humans.Web/Models/Camp/CampRoleRowViewModel.cs
@@ -0,0 +1,15 @@
+namespace Humans.Web.Models.Camp;
+
+public sealed class CampRoleRowViewModel
+{
+    public required Guid DefinitionId { get; init; }
+    public required string Name { get; init; }
+    public required string? Description { get; init; }
+    public required int SlotCount { get; init; }
+    public required int MinimumRequired { get; init; }
+    public required bool IsRequired { get; init; }
+    public required IReadOnlyList<CampRoleSlotViewModel> FilledSlots { get; init; }
+    public required int EmptySlotCount { get; init; }
+    public required bool OverCapacity { get; init; }
+    public required int CurrentCount { get; init; }
+}
