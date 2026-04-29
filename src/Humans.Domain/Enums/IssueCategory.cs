e7c299da93e020f327fafb755898b9d0807daf99
Peter Drier <peter.drier@gmail.com>
2026-04-29 14:20:27 +0200

    feat(issues): add IssueCategory and IssueStatus enums

---
+++ src/Humans.Domain/Enums/IssueCategory.cs
@@ -0,0 +1,11 @@
+namespace Humans.Domain.Enums;
+
+/// <summary>
+/// What kind of issue this is. Stored as string in DB.
+/// </summary>
+public enum IssueCategory
+{
+    Bug,
+    Feature,
+    Question
+}
