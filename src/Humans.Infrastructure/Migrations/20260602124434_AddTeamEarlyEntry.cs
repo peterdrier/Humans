18850ecdf5e17076fc8441b724c20ed7fc329e06
Frank <fanteevi@gmail.com>
2026-06-02 14:45:10 +0200

    feat(teams): migration AddTeamEarlyEntry (grants table + flag)

---
+++ src/Humans.Infrastructure/Migrations/20260602124434_AddTeamEarlyEntry.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace Humans.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeamEarlyEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EarlyEntryEnabled",
+                table: "teams",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "team_early_entry_grants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TeamId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    EntryDate = table.Column<LocalDate>(type: "date", nullable: false),
+                    ProjectName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    CreatedAt = table.Column<Instant>(type: "timestamp with time zone", nullable: false),
+                    CreatedByUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UpdatedAt = table.Column<Instant>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_team_early_entry_grants", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_team_early_entry_grants_teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "teams",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0001-000000000001"),
+                columns: new string[0],
+                values: new object[0]);
+
+            migrationBuilder.UpdateData(
+                table: "teams",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0001-000000000002"),
+                columns: new string[0],
+                values: new object[0]);
+
+            migrationBuilder.UpdateData(
+                table: "teams",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0001-000000000003"),
+                columns: new string[0],
+                values: new object[0]);
+
+            migrationBuilder.UpdateData(
+                table: "teams",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0001-000000000004"),
+                columns: new string[0],
+                values: new object[0]);
+
+            migrationBuilder.UpdateData(
+                table: "teams",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0001-000000000005"),
+                columns: new string[0],
+                values: new object[0]);
+
+            migrationBuilder.UpdateData(
+                table: "teams",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0001-000000000006"),
+                columns: new string[0],
+                values: new object[0]);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_team_early_entry_grants_TeamId",
+                table: "team_early_entry_grants",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_team_early_entry_grants_UserId",
+                table: "team_early_entry_grants",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "team_early_entry_grants");
+
+            migrationBuilder.DropColumn(
+                name: "EarlyEntryEnabled",
+                table: "teams");
+        }
+    }
+}
